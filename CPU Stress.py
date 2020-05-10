7647ee3468d96aa43389d3b9069ade63fd22ec98
SebastiánEPH <sebastianeph99@gmail.com>
2020-05-10 16:49:02 -0500

    CPU Stress 1.0 Finish

---
+++ CPU Stress.py
@@ -0,0 +1,30 @@
+# v1.0
+from multiprocessing import Process
+import random
+          
+def CPU():
+    def sature():
+        n1 =  (random.randrange(98798498456498889)/random.randrange(15))
+        n2 =  (random.randrange(98798498456498889)+random.randrange(64165143651651))
+        n3 =  (random.randrange(98798498456498889)*random.randrange(999))
+        n4 =  (random.randrange(98798498456498889)*random.randrange(453))
+        n5 =  (random.randrange(98798498456498889)-random.randrange(453453453453453453))
+        n6 =  (random.randrange(98798498456498889)*random.randrange(1435))
+        n7 =  (random.randrange(98798498456498889)*random.randrange(4534))
+        n8 =  (random.randrange(98798498456498889)-random.randrange(45345453453453453))
+        n9 =  (random.randrange(98798498456498889)*random.randrange(154345))
+        n10 = (random.randrange(98798498456498889)*random.randrange(4354345345345))
+    while(True):
+        try:
+            while(True):
+                sature()
+        except:
+            pass   
+if __name__ == '__main__':
+    
+        while(True):
+            cpuS = Process(target=CPU)
+            cpuS.start()
+            
+# By SebastianEPH
+
